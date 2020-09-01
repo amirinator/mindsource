b96046fccb42f0ac85a3e0d03b9a09e78b3f71ea
Amirinator <969883+amirinator@users.noreply.github.com>
2020-09-01 14:54:14 -0700

    Updated code changes: - Created stand alone applications for Visit and Encryption exercise - Created service and implementations according to directory structure - New models for storing Visit and Message information

---
+++ src/main/java/com/amirinator/application/EncryptionApplication.java
@@ -0,0 +1,19 @@
+package com.amirinator.application;
+
+import com.amirinator.model.Message;
+import com.amirinator.service.EncryptionService;
+import com.amirinator.service.impl.EncryptionServiceImpl;
+
+public class EncryptionApplication {
+
+	public static void main(String[] args) {
+
+		EncryptionService service = new EncryptionServiceImpl();
+		Integer columnWidth = Integer.valueOf(args[0]);
+		String encryptedMessage = args[1];
+		Message message = new Message(encryptedMessage, columnWidth);
+		Message decryptedMessage = service.decrypt(message);
+		System.out.println("The encrypted message \"" + encryptedMessage + "\" is decrypted as \""
+				+ decryptedMessage.getContent() + "\"");
+	}
+}
